79418cc6ec4dd0a379201da3bb4e113a3f27407b
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-06-26 19:17:35 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicioSelectores';
+  valor1!:number;
+  valor2!:number;
+  resultado!:number;
+
+  operacionSeleccionada: string = 'suma';
+  tipoOperaciones = [
+    'suma',
+    'resta',
+    'multiplicacion',
+    'division',
+  ];
+
+  operar() {
+    switch (this.operacionSeleccionada) {
+      case 'suma' : this.resultado = this.valor1 + this.valor2;
+                    break;
+      case 'resta' : this.resultado = this.valor1 - this.valor2;
+                     break;
+      case 'multiplicacion' : this.resultado = this.valor1 * this.valor2;
+                              break;
+      case 'division' : this.resultado = this.valor1 / this.valor2;
+                        break;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
